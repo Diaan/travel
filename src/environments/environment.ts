bbeb1941f6e42df6b80c1633959a70d3875f8d1b
Diaan <info@dianabroeders.nl>
2018-07-18 21:47:20 +0200

    TA-1 compilation fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB-iwi4aPvoyljcrpm2Fui4IbiL5aiEwLk',
+    authDomain: 'travel-10487.firebaseapp.com',
+    databaseURL: 'https://travel-10487.firebaseio.com',
+    projectId: 'travel-10487',
+    storageBucket: 'travel-10487.appspot.com',
+    messagingSenderId: '684314036912'
+  }
 };
+
+
 
 /*
  * In development mode, to ignore zone related error stack frames such as
