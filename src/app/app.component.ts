ef9939b875ec71cacef259d5ecd0e6ee40a9a041
Diaan <info@dianabroeders.nl>
2018-07-18 21:38:58 +0200

    TA-1 switched to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'travel app';
+  items: Observable<any[]>;
+
+  constructor () {
+    // this.items = db.collection('items').valueChanges();
+  }
 }
