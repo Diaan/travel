e84dae5cea6630a091613577a70a12f8341b3b67
Diaan <info@dianabroeders.nl>
2018-07-23 21:33:42 +0200

    TA-6: needed to be checked in in order to build

---
+++ src/app/day/day-description/day-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ta-day-description',
+  templateUrl: './day-description.component.html',
+  styleUrls: ['./day-description.component.css']
+})
+export class DayDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/day/day-photos/day-photos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ta-day-photos',
+  templateUrl: './day-photos.component.html',
+  styleUrls: ['./day-photos.component.css']
+})
+export class DayPhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
